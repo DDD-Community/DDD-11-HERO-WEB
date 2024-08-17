25e9137f18b59d1eece7cf2b36fb53793dbde656
G-hoon <new0126@naver.com>
2024-08-18 04:48:04 +0900

    [feat/#19] 메인 화면 레이아웃 구성 및 개발
    - 메인화면 마크업
    - 가이드 팝업 추가
    - 카메라 좌우 반전

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,32 +1,15 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import SideNav from "@/components/SideNav"
 
 const Layout: React.FC = () => {
   return (
     <div className="flex min-h-screen w-screen bg-gray-100">
       <div className="flex w-full">
-        <aside className="w-[224px] flex-none flex-shrink-0 bg-[#1C1D20]">
-          <nav className="p-4">
-            <ul>
-              <li className="mb-2">
-                <a href="#home" className="text-gray-700">
-                  Home
-                </a>
-              </li>
-              <li className="mb-2">
-                <a href="#about" className="text-gray-700">
-                  About
-                </a>
-              </li>
-              <li className="mb-2">
-                <a href="#contact" className="text-gray-700">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <main className="min-w-[652px] flex-1 overflow-y-auto bg-[#1C1D20] p-[6px]">
+        <SideNav />
+
+        {/* Main Content */}
+        <main className="min-w-[652px] flex-1 overflow-y-auto bg-[#1C1D20] p-1.5">
           <Outlet />
         </main>
       </div>
