e9a42ecdc506dfd07c1f7b74d348e86c5ffa24de
lkh14011424@gmail.com <lkh14011424@gmail.com>
2024-08-20 03:54:48 +0900

    [feat/#16] 불필요 로직 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import MainCraftIcon from "public/icons/posture-craft-side-nav-icon.svg?react"
-import { useEffect } from "react"
 
 const REST_API_KEY = import.meta.env.VITE_OAUTH_KAKAO_REST_API_KEY
 const REDIRECT_URI = import.meta.env.VITE_OAUTH_KAKAO_REDIRECT_URI
