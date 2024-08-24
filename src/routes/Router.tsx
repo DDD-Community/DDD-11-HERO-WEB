52c6cbb624cff41fafff683b8abceb555fc8ace9
G-hoon <new0126@naver.com>
2024-08-25 01:06:12 +0900

    [feat/hotfix] 로그인하지 않아도 모니터링 화면에 접근 되도록 임시 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import { AuthPage, MonitoringPage, HomePage } from "@/pages"
 import { Layout } from "@/layouts"
 import RoutePath from "@/constants/routes.json"
-import AuthRoute from "@/routes/AuthRoute"
+// import AuthRoute from "@/routes/AuthRoute" // ToDo: 로그인 정상화 될때까지 routing 보호하지 않도록 수정
 
 const Router: React.FC = () => {
   return (
@@ -13,9 +13,9 @@ const Router: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/" element={<Layout />}>
           {/* AuthRoute로 보호된 경로를 감쌉니다 */}
-          <Route element={<AuthRoute />}>
-            <Route path={RoutePath.MONITORING} element={<MonitoringPage />} />
-          </Route>
+          {/* <Route element={<AuthRoute />}> */}
+          <Route path={RoutePath.MONITORING} element={<MonitoringPage />} />
+          {/* </Route> */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
