ba464514b3adaa8294129fc3c9dc1ca14280faa4
G-hoon <new0126@naver.com>
2024-09-26 01:32:30 +0900

    [feat/#58] 디자인 QA 수정

--- src/components/Crew/MyCrew/MyCrewHeader.tsx
+++ src/components/Crew/MyCrew/MyCrewHeader.tsx
@@ -7,7 +7,7 @@ interface MyCrewHeaderProps {
 export default function MyCrewHeader(props: MyCrewHeaderProps) {
   const { openCreateModal } = props
   return (
-    <div className="mb-[24px] flex w-full items-center">
+    <div className="mb-6 flex w-full items-baseline">
       <div className="flex-grow text-[22px] font-bold text-zinc-900">나의 크루</div>
       <div
         className="flex w-[138px] cursor-pointer items-center justify-center gap-[10px] rounded-[33px] bg-zinc-800 p-[10px] text-sm font-semibold text-white"
