173c4e22684c77afc640fe41f6cf7380d286286a
lkh14011424@gmail.com <lkh14011424@gmail.com>
2024-11-17 22:25:35 +0900

    [feat/#123] 일렉트론 개발 및 빌드 환경 세팅, 다운로드 페이지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import svgrPlugin from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import svgrPlugin from "vite-plugin-svgr"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgrPlugin()],
   server: {
     port: 3000,
   },
-});
+  base : "./"
+})
